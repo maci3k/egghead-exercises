07dc767526de3685e4f59d46ee10ad89850c71d5
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-02 12:42:36 +0200

    Task completed

--- app/filter.js
+++ app/filter.js
@@ -4,8 +4,35 @@
     var app = angular.module('app', []);
 
 //add quote filter
+    app.filter('quote', function() {
+        return function(text) {
+            return '\"' + text + '\"';
+        };
+    });
 //add withoutH filter
+    app.filter('withoutH', function() {
+        return function(text) {
+            return text.split(/h|H/).join('');
+        };
+    });
 //add firstLetterUp filter
+    app.filter('firstLetterUp', function() {
+        return function(text) {
+            var array = text.split(' ');
+
+            for(var i=0; i<array.length; i++) {
+                array[i] = array[i].substring(0, 1).toUpperCase() + array[i].substring(1);
+            }
+
+            array = array.join(' ').split('-');
+
+            for(i=0; i<array.length; i++) {
+                array[i] = array[i].substring(0, 1).toUpperCase() + array[i].substring(1);
+            }
+
+            return array.join('-');
+        };
+    });
 
     app.controller('FilterCtrl', function ($scope)
     {
