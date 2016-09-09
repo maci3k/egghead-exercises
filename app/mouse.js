7f2553c1c6fee60a2c4facdce941ec957575a0ee
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-09 12:26:49 +0200

    Task completed

--- app/mouse.js
+++ app/mouse.js
@@ -2,6 +2,37 @@
 {
     'use strict';
 
-    angular.module('app', []);
+    var app = angular.module('app', []);
 
+    app.directive('enter', function() {
+        return function(scope, element, attrs) {
+            element.bind('mouseenter', function() {
+                element.addClass(attrs.enter);
+            });
+        };
+    });
+
+    app.directive('leave', function() {
+        return function(scope, element, attrs) {
+            element.bind('mouseleave', function() {
+                element.removeClass(attrs.leave);
+            });
+        };
+    });
+
+    app.directive('clickOn', function() {
+        return function(scope, element, attrs) {
+            element.bind('click', function() {
+                element.addClass(attrs.clickOn);
+            });
+        };
+    });
+
+    app.directive('dbClickOn', function() {
+        return function(scope, element, attrs) {
+            element.bind('dblclick', function() {
+                element.removeClass(attrs.dbClickOn);
+            });
+        };
+    });
 })();
