392b9be68a6c148f9f833f5ff7acedbbcd1ee144
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-13 12:30:14 +0200

    Task completed

--- app/port.js
+++ app/port.js
@@ -8,6 +8,7 @@
 //  checkMyBoat function should be used to check if boat is able to depart from port
         $scope.checkMyBoat = function (boat, conditions)
         {
+            //alert(boat + ' ' + conditions);
             switch (conditions) {
                 case 'stormy':
                     return false;
@@ -31,12 +32,18 @@
     app.directive('shipSender', function ()
     {
         return {
+            restrict: 'E',
+            scope: {
+                customCheck: '&',
+                conditions: '@'
+            },
             template: '<div class="ship relative">' +
-            '<div class="bubble" ng-if="answer">Set sails!</div>' + //value from the select element below should be used to set
-                                                                    // boat model and passed as argument to correct function
+            '<div class="bubble" ng-if="answer">Set sails!</div>' + '{{boat}}' +    //value from the select element below should be used to set
+                                                                                    // boat model and passed as argument to correct function
             '<div class="col-md-offset-1 col-md-10 align-bottom">' +
-            '<select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']"></select>' +
-            '<div id="checkButton" class="btn btn-info voffset">Check<!--check button--></div></div></div>'
+            '<select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']" ng-model="boat"></select>' +
+            '<div id="checkButton" class="btn btn-info voffset"' +
+            'ng-click="answer = customCheck({boat:boat, currentConditions: conditions})">Check<!--check button--></div></div></div>'
         };
     });
 })();
