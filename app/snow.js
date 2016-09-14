b3c499b9fce2183dbd24c29f77364e8c034a04ba
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-14 11:54:49 +0200

    Task completed

--- app/snow.js
+++ app/snow.js
@@ -8,9 +8,24 @@
     {
         $scope.snow = [];
 
+        var snow = '';
+
 //    addPetal function
+        $scope.addPetal = function() {
+            $scope.snow.push(snow);
+        };
 //    removePetal function
+        $scope.removePetal = function() {
+            $scope.snow.pop();
+        };
     });
 
 //    add enter directive
+    app.directive('enter', function() {
+        return function (scope, element, attrs) {
+            element.bind('mouseenter', function () {
+                scope.$apply(attrs.enter);
+            });
+        };
+    });
 })();
