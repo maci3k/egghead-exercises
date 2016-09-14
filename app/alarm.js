cf75ac9fb4181ffe991d7feacfaee672c1436d1a
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-14 13:02:00 +0200

    Task completed

--- app/alarm.js
+++ app/alarm.js
@@ -1,11 +1,58 @@
 (function ()
 {
     'use strict';
-    angular.module('app', []);
+    var app = angular.module('app', []);
 
-    //add alarm directive
-    //add red directive
-    //add yellow directive
-    //add green directive
+    app.directive('alarm', function() {
+        return {
+            restrict: 'A',
+            controller: function () {
+                this.addRed = function () {
+                    alert('Red alarm!');
+                };
+
+                this.addYellow = function () {
+                    alert('Yellow alarm!');
+                };
+
+                this.addGreen = function () {
+                    alert('Green alarm!');
+                };
+            }
+        };
+    });
+
+    app.directive('red', function() {
+        return {
+            require: 'alarm',
+            link: function (scope, element, attrs, alarmCtrl) {
+                element.bind('click', function () {
+                    alarmCtrl.addRed();
+                });
+            }
+        };
+    });
+
+    app.directive('yellow', function() {
+        return {
+            require: 'alarm',
+            link: function (scope, element, attrs, alarmCtrl) {
+                element.bind('dblclick', function () {
+                    alarmCtrl.addYellow();
+                });
+            }
+        };
+    });
+
+    app.directive('green', function() {
+        return {
+            require: 'alarm',
+            link: function (scope, element, attrs, alarmCtrl) {
+                element.bind('mouseenter', function () {
+                    alarmCtrl.addGreen();
+                });
+            }
+        };
+    });
 
 })();
