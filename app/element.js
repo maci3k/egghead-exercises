8a9832b79e8851fe6a20792011d30a3f08f461f0
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-19 10:18:22 +0200

    Task completed

--- app/element.js
+++ app/element.js
@@ -6,21 +6,46 @@
 
     element.directive('findElement', function ()
     {
+        var h4 = angular.element('<h4></h4>');
 
-        //complete link function
         var link = function (scope)
         {
-            //watch changes on input
+            scope.$watch('input', function(value) {
+                switch(value) {
+                    case 'circle':
+                        h4.removeClass('text-danger');
+                        h4.removeClass('square');
+                        h4.text('');
 
+                        h4.addClass('circle');
+                        break;
+                    case 'square':
+                        h4.removeClass('text-danger');
+                        h4.removeClass('circle');
+                        h4.text('');
+
+                        h4.addClass('square');
+                        break;
+                    default:
+                        h4.removeClass('circle');
+                        h4.removeClass('square');
+
+                        h4.addClass('text-danger');
+                        h4.text('You did not enter *circle* or *square*');
+                        break;
+                }
+            });
         };
 
         return {
+            retrict: 'E',
             replace: true,
             template: '<div> <input class="form-control" type="text" ng-model="input" placeholder="Write circle or square"></div>',
             compile: function (templateElement)
             {
+                templateElement.append(h4);
 
-                //return link function
+                return link;
             }
         };
     });
